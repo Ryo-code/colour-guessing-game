dcde2c7ad279a0950eb2d716df2cd427b24e35fa
Ryo <kanadajinlee@yahoo.ca>
2017-05-16 13:50:22 -0500

    I have some logic going for if you click the right/wrong square, with hardcoded colours

--- colourGame.js
+++ colourGame.js
@@ -8,8 +8,24 @@ var colours = [
 ]
 
 var squares = document.querySelectorAll(".square")
+var pickedColour = colours[3];
+var colourDisplay = document.getElementById("colourDisplay")
+
+colourDisplay.textContent = pickedColour;
 
 for(var i = 0; i < squares.length; i++){
+  //add initial colours to squares
   squares[i].style.background = colours[i]
 
+  //add click listeners to squares
+  squares[i].addEventListener("click", function(){
+    var clickedColour = this.style.background;
+    if(clickedColour === pickedColour){
+      alert("Correct!");
+    }else{
+      this.style.background = "#232323"
+    }
+  });
 }
+
+
