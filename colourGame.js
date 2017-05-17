4d62dfe91f4cd072ff0eac63fa08f0c993c6bbe6
Ryo <kanadajinlee@yahoo.ca>
2017-05-17 14:58:57 -0500

    Got the easy button to show only 3 squares, but need to improve functionality after you get a correct answer

--- colourGame.js
+++ colourGame.js
@@ -1,4 +1,5 @@
-var colours = generateRandomColours(6);
+var numSquares = 6;
+var colours = generateRandomColours(numSquares);
 
 var squares = document.querySelectorAll(".square")
 var pickedColour = pickColour();
@@ -6,11 +7,57 @@ var colourDisplay = document.getElementById("colourDisplay")
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+var ridiculousBtn = document.querySelector("#ridiculousBtn");
+
+easyBtn.addEventListener("click", function(){
+  easyBtn.classList.add("selected");
+  hardBtn.classList.remove("selected");
+  ridiculousBtn.classList.remove("selected");
+  numSquares = 3;
+  colours = generateRandomColours(numSquares);
+  pickedColour = pickColour();
+  colourDisplay.textContent = pickedColour;
+
+  for (var i = 0; i < squares.length; i++){
+    if(colours[i]){
+      squares[i].style.background = colours[i];
+    }else{
+      squares[i].style.background = "none";
+    }
+  }
+})
+
+hardBtn.addEventListener("click", function(){
+  easyBtn.classList.remove("selected");
+  hardBtn.classList.add("selected");
+  ridiculousBtn.classList.remove("selected");
+//refactor later!
+  numSquares = 6;
+  colours = generateRandomColours(numSquares);
+  pickedColour = pickColour();
+  colourDisplay.textContent = pickedColour;
+
+  for (var i = 0; i < squares.length; i++){
+    squares[i].style.background = colours[i];
+    squares[i].style.display = "block";
+  }
+})
+
+ridiculousBtn.addEventListener("click", function(){
+  easyBtn.classList.remove("selected");
+  hardBtn.classList.remove("selected");
+  ridiculousBtn.classList.add("selected");
+
+  numSquares = 9;
+  colours = generateRandomColours(numSquares);  
+})
 
 resetButton.addEventListener("click", function(){
-  colours = generateRandomColours(6);       //generate all new colours
-  pickedColour = pickColour();              //pick a new random colour from Aaray
-  colourDisplay.textContent = pickedColour; //chage colours of squares
+  colours = generateRandomColours(numSquares);  //generate all new colours
+  pickedColour = pickColour();                  //pick a new random colour from Aaray
+  colourDisplay.textContent = pickedColour;     //chage colours of squares
   for(var i = 0; i < squares.length; i++){
     squares[i].style.background = colours[i];
   }
@@ -20,14 +67,6 @@ resetButton.addEventListener("click", function(){
 
 colourDisplay.textContent = pickedColour;
 
-function changeColoursToCorrectAnswer(colour){
-  //loop through all squares
-  for (var i = 0; i < squares.length; i++){
-    //change each colour to match given colour
-    squares[i].style.background = colour;
-  }
-}
-
 for(var i = 0; i < squares.length; i++){
   squares[i].style.background = colours[i];  //add initial colours to squares
 
@@ -35,19 +74,25 @@ for(var i = 0; i < squares.length; i++){
     var clickedColour = this.style.background;  //grab colour of clicked square
     if(clickedColour === pickedColour){         //compare colour & pickedColour
       messageDisplay.textContent = "Correct!";
+      resetButton.textContent = "Play Again";
       changeColoursToCorrectAnswer(clickedColour);
       h1.style.background = clickedColour;
-      resetButton.textContent = "Play Again";
     }else{
       this.style.background = "#232323";
-      messageDisplay.textContent = "Not quite";
+      messageDisplay.textContent = "Try Again";
     }
   });
 }
 
+function changeColoursToCorrectAnswer(colour){
+  for (var i = 0; i < squares.length; i++){  //loop through all squares
+    squares[i].style.background = colour;    //change each colour to match given colour
+  }
+}
+
 function pickColour(){
-  //Picks one of the coloured squares at random (Math.Floor() cuts out the decimals I think)
   var random = Math.floor(Math.random() * colours.length); 
+  //Picks one of the coloured squares at random (Math.Floor() cuts out the decimals I think)
   return colours[random];
 }
 
@@ -60,12 +105,8 @@ function generateRandomColours(num){
 }
 
 function randomColour(){
-  //pick a "red" from 0 - 255
-  var r = Math.floor(Math.random() * 256)
-  //pick a "green" from 0 - 255
-  var g = Math.floor(Math.random() * 256)
-  //pick a "blue" from 0 - 255
-  var b = Math.floor(Math.random() * 256)
-  return "rgb(" + r + ", " + g + ", " + b + ")"
-  
+  var r = Math.floor(Math.random() * 256);  //pick a "red" from 0 - 255
+  var g = Math.floor(Math.random() * 256);  //pick a "green" from 0 - 255
+  var b = Math.floor(Math.random() * 256);  //pick a "blue" from 0 - 255
+  return "rgb(" + r + ", " + g + ", " + b + ")";
 }
