8ba35772e5c8e4b465a3154b7705acb64221403d
Ryo <kanadajinlee@yahoo.ca>
2017-05-16 21:36:18 -0500

    Got it so that the squares all change colour to the correct answer's colour

--- colourGame.js
+++ colourGame.js
@@ -10,8 +10,17 @@ var colours = [
 var squares = document.querySelectorAll(".square")
 var pickedColour = colours[3];
 var colourDisplay = document.getElementById("colourDisplay")
+var messageDisplay = document.querySelector("#message");
 
 colourDisplay.textContent = pickedColour;
+
+function changeColoursToCorrectAnswer(colour){
+  //loop through all squares
+  for (var i = 0; i < squares.length; i++){
+    //change each colour to match given colour
+    squares[i].style.background = colour;
+  }
+}
 
 for(var i = 0; i < squares.length; i++){
   //add initial colours to squares
@@ -21,11 +30,11 @@ for(var i = 0; i < squares.length; i++){
   squares[i].addEventListener("click", function(){
     var clickedColour = this.style.background;
     if(clickedColour === pickedColour){
-      alert("Correct!");
+      messageDisplay.textContent = "Correct!";
+      changeColoursToCorrectAnswer(clickedColour);
     }else{
-      this.style.background = "#232323"
+      this.style.background = "#232323";
+      messageDisplay.textContent = "Try again ;)"
     }
   });
 }
-
-
